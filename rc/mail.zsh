5190a9beec552701bfd68990fbe6742420385a23
Benoit Plessis <benoit@plessis.info>
2012-10-04 00:58:54 +0200

    start fetchmail when needed

---
+++ rc/mail.zsh
@@ -0,0 +1,24 @@
+# -*- shell-script -*-
+# Start fetchmail automatically if configured
+
+if [[ -x $(which fetchmail) && 
+    ! -f ~/.fetchmail.pid &&
+    ! -f /tmp/.${USER}.fetchmailnoask &&
+      -r $HOME/.fetchmailrc ]]
+then
+    local ans
+    echo -n "Start fetchmail ? [y/n] "
+    read -q ans
+    if [[ $ans == "y" ]]
+    then
+	fetchmail
+    else
+	echo -n "Ask again ? [y/n] "
+	read -q ans
+	if [[ $ans == "n" ]]
+	then
+	    # I use /tmp since it's purge every reboot
+	    echo "" >| /tmp/.${USER}.fetchmailnoask
+	fi
+    fi
+fi
