8fcc28d5c5626ab2e74b6a6e27f022aaf96d61a0
Benoit Plessis <benoit@plessis.info>
2012-10-04 09:25:52 +0200

    disable beeping disable error when no match disable timeout for rm *

--- rc/options.zsh
+++ rc/options.zsh
@@ -1,6 +1,8 @@
 # -*- shell-script -*-
 # Defaults options
 
+setopt nobeep                   # i hate beeps
+
 # Background/sub processes
 setopt nohup                    # and don't kill them, either
 setopt printexitvalue           # show the exit-value if > 0
@@ -10,15 +12,17 @@ setopt interactivecomments      # Allow comments even in interactive shells.
 
 # Filename matching
 setopt extended_glob            # in order to use #, ~ and ^ for filename generation
+setopt no_nomatch		# don't print error if no matches
 
 # Directory stask
 setopt auto_cd                  # a commande like % /usr/local is equivalent to cd /usr/local
 setopt auto_pushd               # automatically append dirs to the push/pop list
+#setopt cdable_vars              # avoid the need for an explicit $
 setopt pushd_minus              # Invert the behavior of cd -<tab> cd +<tab>
 export DIRSTACKSIZE=20		# Number of directory to keep in the stack
 
 setopt correct                  # try to correct the spelling if possible
-setopt rmstarwait               # wait 10 seconds before querying for a rm which contains a *
+#setopt rmstarwait               # wait 10 seconds before querying for a rm which contains a *
 setopt noclobber                # Don't erase file with >, use >| (overwrite) or >> (append) instead
 
 # Set default umask to 027
