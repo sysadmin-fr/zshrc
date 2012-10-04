a9cc5753e1ad8167321c5b316035e71c8bc0d11b
Benoit Plessis <benoit@plessis.info>
2012-10-04 09:22:36 +0200

    Add action's time in watch

--- rc/watch.zsh
+++ rc/watch.zsh
@@ -1,7 +1,10 @@
 # -*- shell-script -*-
 # Watch login / logout
 
-watch=(all)                     # watch for everybody
-LOGCHECK=300                    # check every 5 min for login/logout activity
+LOGCHECK=300                        # check every 5 min for login/logout activity
+watch=(all)                         # watch for everybody
+#watch=( $(<~/.friends) )           # watch for people in .friends file
+#watch=(notme)                      # watch for everybody but me
+WATCHFMT='%n %a %l from %m at %T.'  # Add time to watch msg
 
 return
