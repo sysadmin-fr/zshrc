269c3bd7fb63684c214f1136fcf7e84322125b6b
Benoit Plessis <benoit@doyousoft.com>
2012-10-05 10:55:37 +0200

    Don't start ssh agent if one's already running

--- rc/ssh.zsh
+++ rc/ssh.zsh
@@ -2,7 +2,7 @@
 # Démarrage d'un agent de gestion de clefs ssh si necessaire
 
 # ssh key management
-if [[ "$USER" != "root" ]]; then
+if [[ "$USER" != "root" && -z $SSH_AUTH_SOCK ]]; then
     autoload -U ssh_key_manage
     ssh_key_manage
 fi
