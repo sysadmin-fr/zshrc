e6a1128463b17d0d7c9fe21d4f951e5174bb44f7
Benoit Plessis <b.plessis@doyousoft.com>
2018-06-20 23:19:42 +0200

    allow only ANSI color sequences, render less less-unusable ^^

--- rc/variables.zsh
+++ rc/variables.zsh
@@ -12,7 +12,7 @@ fi
 # less
 if [[ -x $(which less) ]]; then
 	export PAGER=less
-	export LESS="-ir"
+	export LESS="-iR"
 
         if [[ -x $(which lesspipe) ]]; then
             eval $($(which lesspipe))
@@ -44,5 +44,11 @@ export PATH
 # Remove '/' from included word chars
 WORDCHARS="${WORDCHARS:s#/#}"
 WORDCHARS="${WORDCHARS:s#=#}"
+
+# Go paths
+GOBIN="${HOME}/.local/bin/"
+GOPATH="${HOME}/.local/share/go:${HOME}/travail/devs/go/"
+GOCACHE="${HOME}/.cache/go"
+export GOBIN GOPATH GOCACHE
 
 return
